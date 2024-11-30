db8ee73e7b7164afe6b6dd8f50884d47287cab35
Gabriel Joaquim <gabriel.joaquim.5567@gmail.com>
2024-11-30 15:02:11 -0300

    fix:correção de interaface
    corrigidos erros na interface e iniciado backend com weatherviewmodel

---
+++ Api-Clima/Api-Clima/ViewModels/WeatherViewModel.cs
@@ -0,0 +1,49 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api_Clima.ViewModels
+{
+    public class WeatherViewModel
+    {
+        [ObservableProperty]
+        private string cityName;
+
+        [ObservableProperty]
+        private int temp;
+
+        [ObservableProperty]
+        private string image;
+
+        [ObservableProperty]
+        private string geral; //"poucas nuvens" nao sei como vou fazer essa bomba
+
+        [ObservableProperty]
+        private int min;
+        [ObservableProperty]  //min/max sensação de feelsLike
+        private int max;
+        [ObservableProperty]
+        private int feelsLike;
+
+        [ObservableProperty]
+        private float sunrise;
+        [ObservableProperty]
+        private float sunset;
+
+        [ObservableProperty]
+        private int humidade;
+
+        [ObservableProperty]
+        private float vento;
+
+        [ObservableProperty]
+        private float visibilidade;
+
+        [ObservableProperty]
+        private int pressao;
+
+    }
+}
